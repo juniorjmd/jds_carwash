3c4af8de7783c518bdcadf6f360b8f8fed266ab3
josé de jesús domínguez padilla <juniorjmd@gmail.com>
2024-09-16 12:40:34 -0500

    creacion de transferencias entre cuentas

---
+++ src/app/modules/shared/components/categorias-dropdown/empleados-dropdown.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { EmpleadoModel } from 'src/app/models/empleados/empleados.module';
+import { DatosInicialesService } from 'src/app/services/DatosIniciales.services';
+
+@Component({
+  selector: 'elemt-empDropdown',
+  templateUrl: './empleados-dropdown.component.html',
+  styleUrls: ['./empleados-dropdown.component.css']
+})
+export class EmpleadosDropdownComponent implements OnInit{
+
+   options: EmpleadoModel[] = [];
+  @Input() selectedValue: EmpleadoModel| null = null;
+  @Output() selectionChange: EventEmitter<EmpleadoModel> = new EventEmitter(); 
+  @Output() clickBtnRefress: EventEmitter<void> = new EventEmitter(); // Even
+  vueltas :boolean =  false;
+  private inicioService= inject(DatosInicialesService) ;
+  
+  ngOnInit(): void { 
+    console.log('opciones : ',this.options);
+    this.inicioService.currentVendedores.subscribe(vendedores => {
+      if (vendedores) {
+        // Aquí puedes trabajar con el array de vendedores
+        console.log('Vendedores actualizados:', vendedores);
+        this.options = vendedores;
+      }
+    });
+  } 
+
+  selectOption(option: EmpleadoModel) {
+    this.selectedValue = option;
+    this.selectionChange.emit(this.selectedValue); 
+  }
+  triggerExternalFunction() {
+    this.clickBtnRefress.emit(); // Emitir el evento
+  }
+}
